4d25ffb298459252e8e2406d26844451933bdde4
13circle <13circle97@gmail.com>
2020-09-14 00:26:09 +0900

    Updates

---
+++ precache-manifest.40d8fcb2688f4380a2f7649fa249ecd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda7a47c13620d06bddb283736738d3c",
+    "url": "/velopert-react-phone-book/index.html"
+  },
+  {
+    "revision": "c647b68ca77865f1d1f4",
+    "url": "/velopert-react-phone-book/static/css/main.bc5c55b2.chunk.css"
+  },
+  {
+    "revision": "ac249c2fe4efa86c063b",
+    "url": "/velopert-react-phone-book/static/js/2.2a50d84f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/velopert-react-phone-book/static/js/2.2a50d84f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c647b68ca77865f1d1f4",
+    "url": "/velopert-react-phone-book/static/js/main.98f60c42.chunk.js"
+  },
+  {
+    "revision": "55c7c110fb1a8e65ed7f",
+    "url": "/velopert-react-phone-book/static/js/runtime-main.44d24164.js"
+  }
+]);
