0eedf9c71bda9261b0340fb34892a3ef45929380
13circle <13circle97@gmail.com>
2020-09-13 19:21:22 +0900

    Tutorial Ch.6: Manage input state
    Link: https://velopert.com/3634
    
    * handleChange
    - HTML DOM onchange event by using onChange jsx prop
    
    * PhoneForm state
    - Use "Computed Property Names" syntax
      (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#Computed_property_names)
    - Reset after this.props.onCreate
    
    * PhoneForm.props.onCreate
    - Pass state from PhoneForm to App
    - Child-to-Parent modification

---
+++ src/components/PhoneForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class PhoneForm extends Component {
+  state = {
+    name: "",
+    phone: "",
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    // Prevent page reloading
+    e.preventDefault();
+    // Pass state values to the parent by onCreate
+    this.props.onCreate(this.state);
+    // Initialize state
+    this.setState({
+      name: "",
+      phone: "",
+    });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          placeholder="Name"
+          value={this.state.name}
+          onChange={this.handleChange}
+          name="name"
+        />
+        <input
+          placeholder="Phone"
+          value={this.state.phone}
+          onChange={this.handleChange}
+          name="phone"
+        />
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
+}
+
+export default PhoneForm;
